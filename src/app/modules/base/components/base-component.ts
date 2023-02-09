fc62cade42302ffa56d330622e331b81c2d3674d
Mahmoud Alaskalany <mahmoud.ragab@dhofar.gov.om>
2023-02-09 11:25:56 +0400

    format using prettier

--- src/app/modules/base/components/base-component.ts
+++ src/app/modules/base/components/base-component.ts
@@ -1,4 +1,3 @@
-
 import { OnInit, Directive } from '@angular/core';
 import { HttpService } from 'core/services/http/http.service';
 import { AlertService } from 'core/services/alert/alert.service';
@@ -7,24 +6,26 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { TranslationService } from 'core/services/translation/translation.service';
 import { ExportExcelService } from 'shared/services/export-excel/export-excel.service';
 
-
-
-
 @Directive()
 export abstract class BaseComponent implements OnInit {
-
   title = '';
   pageType = '';
 
   abstract get service(): HttpService;
-  get alert(): AlertService { return Shell.Injector.get(AlertService); }
-  get route(): Router { return Shell.Injector.get(Router); }
-  get excel(): ExportExcelService { return Shell.Injector.get(ExportExcelService); }
-  get localize(): TranslationService { return Shell.Injector.get(TranslationService); }
-
-  constructor(private activatedRoute: ActivatedRoute) {
-
+  get alert(): AlertService {
+    return Shell.Injector.get(AlertService);
   }
+  get route(): Router {
+    return Shell.Injector.get(Router);
+  }
+  get excel(): ExportExcelService {
+    return Shell.Injector.get(ExportExcelService);
+  }
+  get localize(): TranslationService {
+    return Shell.Injector.get(TranslationService);
+  }
+
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.title = this.activatedRoute.snapshot.data['title'];
