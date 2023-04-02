ba38730ec9e5f7c39a6521c303fe509414bfc963
mahmoud alaskalany <mhmoudalaskalany@gmail.com>
2023-04-02 04:15:22 +0400

    add storage service

---
+++ src/app/modules/shared/services/storage/storage.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+  constructor() {}
+
+  setItem(key: string, value: string) {
+    return of(localStorage.setItem(key, value));
+  }
+
+  getItem(key: string) {
+    return of(localStorage.getItem(key));
+  }
+  delete = (key: string) => {
+    localStorage.removeItem(key);
+  };
+
+  getUserData(key: string) {
+    return localStorage.getItem(key);
+  }
+
+  getToken() {
+    return localStorage.getItem('token');
+  }
+  getTokenFromSessionStorage() {
+    return sessionStorage.getItem('token');
+  }
+
+  clearStorage() {
+    return of(localStorage.clear());
+  }
+  clearSessionStorage() {
+    return of(sessionStorage.clear());
+  }
+}
