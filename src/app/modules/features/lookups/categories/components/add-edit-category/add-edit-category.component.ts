91fc780a3b49fa47a6a4016eaea96109dfc463d9
Mahmoud Alaskalany <mhmoudalaskalany@gmail.com>
2023-06-11 03:38:59 +0400

    modify the lookup edit component

--- src/app/modules/features/lookups/categories/components/add-edit-category/add-edit-category.component.ts
+++ src/app/modules/features/lookups/categories/components/add-edit-category/add-edit-category.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit, Optional } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
+import { Shell } from 'base/components/shell';
 
 import { Category, Shop } from 'shared/interfaces/lookups/lookups';
 import { CategoriesService } from 'shared/services/categories/categories.service';
@@ -14,29 +15,31 @@ import { CategoriesService } from 'shared/services/categories/categories.service
 export class AddEditCategoryComponent implements OnInit {
   pageTitle = '';
   pageType = '';
-  lookupId = '';
-  lookupFormGroup!: FormGroup;
+  id = '';
+  form!: FormGroup;
   categories: Category[] = [];
-  shops: Shop[] = [];
-
+  get categoriesService(): CategoriesService {
+    return Shell.Injector.get(CategoriesService);
+  }
+  get fb(): FormBuilder {
+    return Shell.Injector.get(FormBuilder);
+  }
   constructor(
     private dialogRef: MatDialogRef<AddEditCategoryComponent>,
-    @Optional() @Inject(MAT_DIALOG_DATA) public data: { activatedRoute: ActivatedRoute },
-    private fb: FormBuilder,
-    private categoriesService: CategoriesService
+    @Optional() @Inject(MAT_DIALOG_DATA) public data: { activatedRoute: ActivatedRoute }
   ) {}
 
   ngOnInit(): void {
     this.pageTitle = this.data.activatedRoute.snapshot.firstChild?.data['pageTitle'];
     this.pageType = this.data.activatedRoute.snapshot.firstChild?.data['pageType'];
-    this.lookupId = this.data.activatedRoute.snapshot.firstChild?.paramMap.get('id') as string;
+    this.id = this.data.activatedRoute.snapshot.firstChild?.paramMap.get('id') as string;
 
     this.initFormGroup();
     this.getLookups();
   }
 
   initFormGroup() {
-    this.lookupFormGroup = this.fb.group({
+    this.form = this.fb.group({
       nameEn: ['', Validators.required],
       nameAr: ['', Validators.required],
       code: ['', Validators.required],
@@ -55,12 +58,12 @@ export class AddEditCategoryComponent implements OnInit {
   }
 
   getLookup() {
-    this.categoriesService.getEditCategory(this.lookupId).subscribe((category: any) => this.lookupFormGroup.patchValue(category));
+    this.categoriesService.getEditCategory(this.id).subscribe((category: any) => this.form.patchValue(category));
   }
 
   submit() {
-    if (this.pageType === 'add') this.categoriesService.add(this.lookupFormGroup.value).subscribe(() => this.dialogRef.close());
+    if (this.pageType === 'add') this.categoriesService.add(this.form.value).subscribe(() => this.dialogRef.close());
     if (this.pageType === 'edit')
-      this.categoriesService.update({ id: this.lookupId, ...this.lookupFormGroup.value }).subscribe(() => this.dialogRef.close());
+      this.categoriesService.update({ id: this.id, ...this.form.value }).subscribe(() => this.dialogRef.close());
   }
 }
