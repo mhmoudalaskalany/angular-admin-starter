9c3f9c396fa7cb55dbeeaddd4fd1a1d1983cae90
Mahmoud Alaskalany <mahmoud.ragab@dhofar.gov.om>
2023-11-19 12:36:30 +0400

    update auth gurd

--- src/app/modules/core/guards/auth/auth.guard.ts
+++ src/app/modules/core/guards/auth/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { AuthService } from '../../services/auth/auth.service';
@@ -7,7 +7,7 @@ import { AuthService } from '../../services/auth/auth.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
 
   constructor(private router: Router, private authService: AuthService) { }
 
